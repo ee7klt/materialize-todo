e13b5959f2243c4b54261f1a09f4a4db36fbb390
ee7klt <kltah1@gmail.com>
2015-11-04 14:39:42 +0800

    solved FOUT by moving webfontloader script to first thing in <head>

--- src/app/components/todo-input.jsx
+++ src/app/components/todo-input.jsx
@@ -12,22 +12,22 @@ const TodoInput = React.createClass ({
   handleClick () {
     console.log('pushing '+this.refs.todoInput.getValue()+" to firebase");
     console.log(this.props.todoStore)
-      this.props.todoStore.push(this.refs.todoInput.getValue())
+    this.props.todoStore.push(this.refs.todoInput.getValue())
   },
 
   render () {
     return (
 
       <div className = 'inputStyle'>
-      <h1>Carpe Diem</h1>
-      <TextField
-        floatingLabelText="Add a To Do"
-        ref = "todoInput"
-        />
-      <FloatingActionButton onClick = {this.handleClick}>
+        <h1>Carpe Diem</h1>
+        <TextField
+          floatingLabelText="Add a To Do"
+          ref = "todoInput"
+          />
+        <FloatingActionButton onClick = {this.handleClick}>
         </FloatingActionButton>
       </div>
-);
+    );
   }
 
 
